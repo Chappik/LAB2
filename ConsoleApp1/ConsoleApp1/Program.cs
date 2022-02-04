c111861c93d1931808aa455a4636c40d2f0d32ec
Chappik <artemijeorel@yandex.ru>
2022-02-04 16:38:08 +0300

    111

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,3 +2,51 @@
 
 
 
+
+
+int a = 0, b = 0, vvod;
+
+
+
+while (true)
+
+{
+
+
+    Console.WriteLine("1-Ввести A\n2-Ввести B\n3-Выполнить (+)\n4-Выполнить (-)\n5-Выполнить (*)\n6-Выполнить (/)");
+
+
+    vvod = Convert.ToInt32(Console.ReadLine());
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if (vvod == 5)
+    {
+
+
+        Console.WriteLine(a * b);
+
+
+    }
+
+
+
+}
+
+
