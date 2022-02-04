cc3be1bef14255f54d3c629374639e5ca52c84b1
Chappik <artemijeorel@yandex.ru>
2022-02-04 15:32:57 +0300

    VvodA

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a = 0, b = 0,vvod;
+
+
+
+while(true)
+
+{
+
+
+    Console.WriteLine("1-Ввести A\n2-Ввести B\n3-Выполнить (+)\n4-Выполнить (-)\n5-Выполнить (*)\n6-Выполнить (/)");
+
+
+    vvod = Convert.ToInt32(Console.ReadLine());
+
+
+
+    if(vvod == 1)
+    {
+
+
+        Console.WriteLine("Введите A");
+
+
+        a = Convert.ToInt32(Console.ReadLine());
+
+    }
+   
+
+}
