6d0a9fa431330bdc23edf00e05aba99ede2e1d84
Chappik <artemijeorel@yandex.ru>
2022-02-04 15:52:44 +0300

    C:/Program Files/Git/

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// See https://aka.ms/new-console-template for more information
+int a = 0, b = 0, vvod;
+
+
+
+while (true)
+
+{
+
+
+    Console.WriteLine("1-Ввести A\n2-Ввести B\n3-Выполнить (+)\n4-Выполнить (-)\n5-Выполнить (*)\n6-Выполнить (/)");
+
+
+    vvod = Convert.ToInt32(Console.ReadLine());
+
+
+
+    if (vvod == 6)
+    {
+
+
+        Console.WriteLine(a / b);
+
+
+    }
+
+
+}
