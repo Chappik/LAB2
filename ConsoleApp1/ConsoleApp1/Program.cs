fee136cbc717a494c9afc868fb65b4acf53c1640
Chappik <artemijeorel@yandex.ru>
2022-02-04 16:00:04 +0300

    ff

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -71,33 +71,3 @@ while(true)
 
 }
 
-=======
-﻿// See https://aka.ms/new-console-template for more information
-int a = 0, b = 0, vvod;
->>>>>>> Mnozh
-
-
-
-while (true)
-
-{
-
-
-    Console.WriteLine("1-Ввести A\n2-Ввести B\n3-Выполнить (+)\n4-Выполнить (-)\n5-Выполнить (*)\n6-Выполнить (/)");
-
-
-    vvod = Convert.ToInt32(Console.ReadLine());
-
-
-
-    if (vvod == 5)
-    {
-
-
-        Console.WriteLine(a*b);
-
-
-    }
-
-
-}
